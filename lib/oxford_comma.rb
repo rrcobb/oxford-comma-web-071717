2dea6fca21e2e2b5bd23dc45c8720a3284dca62f
Rob Cobb <rwcobbjr@gmail.com>
2018-02-25 00:04:33 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.first
+  when 2
+    array.join(' and ')
+  else
+    "#{array.slice(0, array.length - 1).join(', ')}, and #{array.last}"
+  end
 end
